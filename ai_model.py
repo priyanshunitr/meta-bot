34f5f30b5e6ec921236124eeb6ae6cd0cddc2790
priyanshunitr <sahupriyanshu2006@gmail.com>
2026-03-31 14:12:52 +0530

    basic model

---
+++ ai_model.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, Response, Request
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from dotenv import load_dotenv
+import asyncio
+
+load_dotenv ()
+
+model = ChatOpenAI()
+
+chat_history = [
+    SystemMessage(content = 'You are a helpful assistant')
+]
+
+template = ChatPromptTemplate([
+    ('system', 'You are a helpful AI assistant'),
+    MessagesPlaceholder ( variable_name = 'recorded_chats')
+])
+
+async def generate_reply(message: str):
+
+    chat_history.append(HumanMessage(content="User: " + message))
+    prompt = template.invoke({'recorded_chats': chat_history })
+
+    result = await model.ainvoke (prompt)
+
+    chat_history.append(AIMessage(content=("Bot: " + result.content)))
+
+    return result.content
+
+print(asyncio.run(generate_reply("Who are you?")))
